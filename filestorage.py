7123edf3b8dc60917c0475b2ea9e76e3d47c7744
frazarshad <fraz.arshad12@gmail.com>
2021-03-17 00:49:52 +0500

    Initial Commit

---
+++ filestorage.py
@@ -0,0 +1,55 @@
+from kademlia.storage import IStorage
+import csv
+import time
+
+class FileStorage(IStorage):
+    def __init__(self, file="kademlia.csv"):
+        f = open(file, "w")
+        f.close()
+        self.file = file
+
+    def cull(self):
+        1
+
+    def __setitem__(self, key, value):
+        with open(self.file, 'a', newline='') as csvfile:
+                spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+                spamwriter.writerow([key, value, time.monotonic()])
+
+    def get(self, key, default=None):
+        with open(self.file, newline='') as csvfile:
+                spamreader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+                for row in spamreader:
+                        if row[0] == str(key):
+                                return row[1]
+        return default
+
+    def __getitem__(self, key):
+        with open(self.file, newline='') as csvfile:
+                spamreader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+                for row in spamreader:
+                        if row[0] == str(key):
+                                return row[1]
+
+    def iter_older_than(self, seconds_old):
+        min_birthday = time.monotonic() - seconds_old
+        ikeys = list()
+        ivalues = list()
+        with open(self.file, newline='') as csvfile:
+                spamreader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+                for row in spamreader:
+                        if min_birthday >= row[2]:
+                                ikeys.append(row[0])
+                                ivalues.append(row[1])
+        return zip(ikeys, ivalues)
+
+    def __iter__(self):
+        self.cull()
+        ikeys = list()
+        ivalues = list()
+        with open(self.file, newline='') as csvfile:
+                spamreader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+                for row in spamreader:
+                        ikeys.append(row[0])
+                        ivalues.append(row[1])
+        return zip(ikeys, ivalues)
