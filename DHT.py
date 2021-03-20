57b916b7f67f7211cedacbf9513a5b72e2c832cd
frazarshad <fraz.arshad12@gmail.com>
2021-03-20 16:47:48 +0500

    First version of dht class

---
+++ DHT.py
@@ -0,0 +1,34 @@
+import sys
+import logging
+import asyncio
+from filestorage import FileStorage
+from kademlia.network import Server
+
+class DHT:
+    def __init__(self, storage_file, port):
+        if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
+            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+        # Logging
+        self.handler = logging.StreamHandler()
+        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.handler.setFormatter(self.formatter)
+        self.log = logging.getLogger('kademlia')
+        self.log.addHandler(self.handler)
+        self.log.setLevel(logging.DEBUG)
+
+        # Set loop
+        self.loop = asyncio.get_event_loop()
+        self.loop.set_debug(True)
+
+        self.node = Server(storage=FileStorage(storage_file))
+        self.loop.run_until_complete(self.node.listen(port))
+
+    def run(self):
+        try:
+            self.loop.run_forever()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.node.stop()
+            self.loop.close()
--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-import sys
-import asyncio
-from filestorage import FileStorage
-from kademlia.network import Server
+from DHT import DHT
 
-if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
-async def run():
-    # Create a node and start listening on port 5678
-    node = Server(storage=FileStorage("kademlia.csv"))
-    await node.listen(5678)
-
-    # Bootstrap the node by connecting to other known nodes, in this case
-    # replace 123.123.123.123 with the IP of another node and optionally
-    # give as many ip/port combos as you can for other nodes.
-    await node.bootstrap([("192.168.18.3", 5678)])
-
-    # set a value for the key "my-key" on the network
-    await node.set("my-key", "my awesome value")
-
-    # get the value associated with "my-key" from the network
-    result = await node.get("my-key")
-    print(result)
-
-asyncio.run(run())
+dht = DHT('kademlia.csv', 5678)
+dht.run()
